579b6919bf8ce75979a98e8dc3e953dc3f95c201
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2025-11-03 03:27:44 +0900

    chore: 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,27 @@
+import Layout from "./pages/Layout";
+import QnAPage from "./pages/QnAPage";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/*
+          / -> login
+          /home -> 강의 목록
+          /:lectureId/attendance -> 강의 출석
+          /:lectureId/attendance/result -> 강의 출석 결과
+          /:lectureId/qna -> QnA 화면
+        */}
+        <Route element={<Layout />}>
+          <Route path="/:lectureId/qna" element={<QnAPage />} />
+          <Route path="/:lectureId/attendance" element={<QnAPage />} />
+          <Route path="/:lectureId/attendance/result" element={<QnAPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
